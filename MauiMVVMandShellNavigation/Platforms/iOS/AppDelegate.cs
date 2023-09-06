78a730804780e70439fe7362e166ee364ccb767b
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-09-06 11:57:47 -0500

    Add project files.

---
+++ MauiMVVMandShellNavigation/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiMVVMandShellNavigation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
